0261a9edc6d32bb6e697ede7379aa5e75dd6c87d
Tessyjames <tessyjames28@gmail.com>
2024-03-24 23:01:19 +0100

    view file

--- fullstack/myproject/myapp/views.py
+++ fullstack/myproject/myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from myapp.forms import CommentForm
-from .models import UserComments
+from myapp.models import UserComments
 from django.http import JsonResponse
 
 # Create your views here.
